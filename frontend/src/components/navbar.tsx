7d56ffb15a3760f4e629df8b61d6e486058a0314
Roshan Kern <roshan.kern@ucdenver.edu>
2025-07-01 08:41:17 -0600

    push logo changes

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -6,11 +6,18 @@ export default function Navbar() {
   return (
     <nav className="mb-6 flex items-center">
       <Image
-        src="/_logo.png"
+        src="/fb_logo.png"
+        alt="Fibrolytix Bio Logo"
+        width={230}
+        height={60}
+      />
+      <h1 className="text-2xl font-bold mx-4">X</h1>
+      <Image
+        src="/gt_logo.png"
         alt="Greenwood Technologies Logo"
         width={60}
         height={60}
-        className="mr-4"
+        className="mr-2"
       />
       <h1 className="text-2xl font-bold">Greenwood Technologies</h1>
     </nav>
